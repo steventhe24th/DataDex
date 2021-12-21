31fef247a2bfef7a2c570f4893a4494741cc17d7
Steven IJ <43561880+steventhe24th@users.noreply.github.com>
2021-12-21 14:30:31 +0700

    update config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Datadex',         # How you named your package folder (MyLib)
   packages = ['Datadex'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Data Science Framework',   # Give a short description about your library
   author = 'steventhe24th',                   # Type in your name
@@ -13,7 +13,10 @@ setup(
   install_requires=[            # I get to this in a second
         'pandas',
         'numpy',
-        'scikit-learn'
+        'scikit-learn',
+        'keras_tuner',
+        'spacy',
+        'tensorflow-text'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
