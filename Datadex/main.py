ea6aa477800fe64988b6706c88c240ed6a7e3212
Steven IJ <43561880+steventhe24th@users.noreply.github.com>
2021-12-23 15:33:54 +0700

    [Utility] add | add zip function to archive files

--- Datadex/main.py
+++ Datadex/main.py
@@ -20,6 +20,7 @@ try:
     from sklearn.metrics import mean_squared_error
     from IPython.display import display, Markdown, HTML, clear_output
     from sklearn.feature_extraction.text import CountVectorizer
+    from zipfile import ZipFile
   
 except Exception as e:
     print(f'[DataDex - LibError] core library not found {e}.\n please run: pip3 install scikit-learn pandas')
@@ -726,3 +727,37 @@ class Utility:
                 continue
             temp_list.append(target_list[i])
         return temp_list
+  
+    def get_all_file_paths(directory):
+        # initializing empty file paths list
+        file_paths = []
+    
+        # crawling through directory and subdirectories
+        for root, directories, files in os.walk(directory):
+            for filename in files:
+                # join the two strings in order to form the full filepath.
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)
+    
+        # returning all file paths
+        return file_paths        
+    
+    def zip_path(path, output_name):
+        # path to folder which needs to be zipped
+        directory = f'./{path}'
+    
+        # calling function to get all file paths in the directory
+        file_paths = get_all_file_paths(directory)
+    
+        # printing the list of all files to be zipped
+        print('Following files will be zipped:')
+        for file_name in file_paths:
+            print(file_name)
+    
+        # writing files to a zipfile
+        with ZipFile(f'{output_name}.zip','w') as zip:
+            # writing each file one by one
+            for file in file_paths:
+                zip.write(file)
+    
+        print('All files zipped successfully!') 
